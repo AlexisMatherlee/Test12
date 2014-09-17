63bc970981d21dfaefc476077fc713338b122b67
Alexis Matherlee <alexismatherlee@gmail.com>
2014-09-17 14:37:11 -0700

    user signup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-
 require 'rubygems'
 require 'spork'
 
@@ -26,6 +23,7 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+  config.mock_with :rspec
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
